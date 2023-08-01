eb41ea5c1f196abe4cfc6c01c36b8ee8366c1edd
Denismikulich91 <denismikulich91@gmail.com>
2023-08-01 18:08:51 +0100

    done classes

--- index.js
+++ index.js
@@ -6,56 +6,8 @@ canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
-class Sprite {
-  constructor({ position, velocity, color='red', offset }) {
-    this.position = position;
-    this.velocity = velocity;
-    this.color = color;
-    this.width = 50;
-    this.height = 150;
-    this.lastKey;
-    this.isAttacking = false;
-    this.attackBox = {
-      position: {
-        x: this.position.x,
-        y: this.position.y,
-      },
-      offset,
-      width: 100,
-      height: 50
-    };
-  this.health = 100;
-  }
-  draw() {
-    c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    if (this.isAttacking) {
-      c.fillStyle = "green";
-      c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
-    }
 
-  }
-  update() {
-    this.draw();
-    this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-    this.attackBox.position.y = this.position.y
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-      this.velocity.y = 0;
-    } else {
-      this.velocity.y += gravity;
-    }
-  }
-  attack() {
-    this.isAttacking = true;
-    setTimeout(() => {
-      this.isAttacking = false;
-    }, 100)
-  }
-}
-
-const player = new Sprite({
+const player = new Fighter({
   position: {
     x: 0,
     y: 0,
@@ -71,7 +23,7 @@ const player = new Sprite({
   }
 });
 
-const enemy = new Sprite({
+const enemy = new Fighter({
   position: {
     x: 400,
     y: 100,
@@ -106,6 +58,32 @@ const keys = {
     pressed: false
   },
 }
+
+const determineWinner = ({player, enemy, timerId}) => {
+  clearTimeout(timerId);
+  document.querySelector('#message').style.display = 'flex'
+  if (player.health === enemy.health) {
+    document.querySelector('#message').innerHTML = 'Tie'
+  } else if (player.health > enemy.health) {
+    document.querySelector('#message').innerHTML = 'Player wins'
+  } else {
+    document.querySelector('#message').innerHTML = 'Enemy wins'
+  }
+}
+
+let timerId;
+let timer = 26;
+const decreaseTimer = () => {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer -= 1;
+    document.querySelector('#timer').innerHTML = timer
+  }
+  if (timer === 0){
+    determineWinner({player, enemy, timerId})
+  }
+}
+decreaseTimer();
 
 const rectangularCollision = ({rectangle1, rectangle2}) => {
   return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
@@ -145,6 +123,11 @@ const animate = () => {
       enemy.isAttacking = false;
       player.health -= 20;
       document.querySelector('#playerHealth').style.width = `${player.health}%`
+  }
+
+  // END THE GAME
+  if (enemy.health === 0 || player.health === 0) {
+    determineWinner({player, enemy, timerId})
   }
 };
 
